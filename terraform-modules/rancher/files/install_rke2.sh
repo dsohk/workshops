1770f70243e0dc9e32be771ccf7fddd1fe71f743
Aki Liu <aki.liu@suse.com>
2023-05-08 16:21:50 +0800

    update

--- terraform-modules/rancher/files/install_rke2.sh
+++ terraform-modules/rancher/files/install_rke2.sh
@@ -1,6 +1,6 @@
 echo "Install Rancher Server using helm chart on RKE2 ..."
 echo "Install RKE2 stable release ..."
-sudo bash -c 'curl -sfL https://get.rke2.io | INSTALL_RKE2_METHOD=tar sh -'
+sudo bash -c 'curl -sfL https://get.rke2.io | INSTALL_RKE2_CHANNEL="v1.25" sh -'
 sudo mkdir -p /etc/rancher/rke2
 sudo bash -c 'echo "write-kubeconfig-mode: \"0644\"" > /etc/rancher/rke2/config.yaml'
 sudo bash -c 'echo "tls-san:" >> /etc/rancher/rke2/config.yaml'
