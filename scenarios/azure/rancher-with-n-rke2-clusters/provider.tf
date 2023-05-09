8ded305b2b94dcaabf098094129df05701385dd1
Aki Liu <aki.liu@suse.com>
2023-05-09 14:46:02 +0800

    update

--- scenarios/azure/rancher-with-n-rke2-clusters/provider.tf
+++ scenarios/azure/rancher-with-n-rke2-clusters/provider.tf
@@ -1,11 +1,10 @@
 provider "azurerm" {
-  features {}
-
   subscription_id = var.azure_subscription_id
-  tenant_id       = var.azure_tenant_id
   client_id       = var.azure_client_id
   client_secret   = var.azure_client_secret
-  version = "=2.88.1"
+  tenant_id       = var.azure_tenant_id
+  version         = "2.88.1"
+  features {}
 }
 
 provider "tls" {
