b1a6ca2798254ffeefed62c53845c8ff0211b3ce
Aki Liu <aki.liu@suse.com>
2023-05-28 22:17:08 +0800

    Update output.tf

--- scenarios/azure/linux-server-cockpit/output.tf
+++ scenarios/azure/linux-server-cockpit/output.tf
@@ -1,4 +1,4 @@
-/* output "linux_server" {
+output "linux_server" {
   sensitive = true
   value = [
     tomap({
@@ -7,8 +7,9 @@
       "pass" = azurerm_linux_virtual_machine.linux_node.*.admin_password,
     })
   ]
-} */
+}
 
+/* 
 output "linux_server_user" {
   value = azurerm_linux_virtual_machine.linux_node.*.admin_username
   description = "User"
@@ -17,14 +18,14 @@ output "linux_server_user" {
 output "linux_server_password" {
   value     = azurerm_linux_virtual_machine.linux_node.*.admin_password
   sensitive = true
-  description = "Password "
+  description = "Password"
 }
 
 output "linux_server_ip" {
   value       = azurerm_linux_virtual_machine.linux_node.*.public_ip_address
   description = "Linux Server IP"
 }
-/* 
+
 output "linux_server_cockput_url" {
   value       = format("https://%s:%s",azurerm_linux_virtual_machine.linux_node.*.public_ip_address,"9090")
   description = "Cockpit Web UI URL"
