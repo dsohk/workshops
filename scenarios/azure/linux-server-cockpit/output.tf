e8e1f66d1a246f7755cffcf97daf34f235244835
Aki Liu <aki.liu@suse.com>
2023-05-28 20:52:06 +0800

    update

--- scenarios/azure/linux-server-cockpit/output.tf
+++ scenarios/azure/linux-server-cockpit/output.tf
@@ -24,8 +24,8 @@ output "linux_server_ip" {
   value       = azurerm_linux_virtual_machine.linux_node.*.public_ip_address
   description = "Linux Server IP"
 }
-
+/* 
 output "linux_server_cockput_url" {
   value       = format("https://%s:%s",azurerm_linux_virtual_machine.linux_node.*.public_ip_address,"9090")
   description = "Cockpit Web UI URL"
-}
+} */
