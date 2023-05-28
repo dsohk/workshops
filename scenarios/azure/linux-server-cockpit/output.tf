4b6a0b3427900e494d0ae06c12e55885606bff28
Aki Liu <aki.liu@suse.com>
2023-05-28 20:37:46 +0800

    output cockpit web url

--- scenarios/azure/linux-server-cockpit/output.tf
+++ scenarios/azure/linux-server-cockpit/output.tf
@@ -26,6 +26,6 @@ output "linux_server_ip" {
 }
 
 output "linux_server_cockput_url" {
-  value       = format("https://%s", azurerm_linux_virtual_machine.linux_node.*.public_ip_address)
+  value       = "https://${azurerm_linux_virtual_machine.linux_node.*.public_ip_address}:9090"
   description = "Cockpit Web UI URL"
 }
