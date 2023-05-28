af06479c957b7e0e88cb156153a247a791ec4a9a
Aki Liu <aki.liu@suse.com>
2023-05-28 23:03:51 +0800

    Update output.tf

--- scenarios/azure/linux-server-cockpit/output.tf
+++ scenarios/azure/linux-server-cockpit/output.tf
@@ -10,7 +10,8 @@ output "linux_server" {
 }
 
 output "linux_server_cockput_url" {
-  value = join("https://",flatten(azurerm_linux_virtual_machine.linux_node.*.public_ip_address),":9090")
+  value = [for no_of_servers  in azurerm_linux_virtual_machine.linux_node :
+  "https://${instance.public_ip_address}:9090"]
   description = "Cockpit Web UI URL"
 } 
 
