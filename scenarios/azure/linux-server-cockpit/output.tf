8f1adccc889b8ce5ea23e7ea7121cbc1a4cae785
Aki Liu <aki.liu@suse.com>
2023-05-28 20:10:10 +0800

    update output

--- scenarios/azure/linux-server-cockpit/output.tf
+++ scenarios/azure/linux-server-cockpit/output.tf
@@ -1,4 +1,4 @@
-output "linux_server" {
+/* output "linux_server" {
   sensitive = true
   value = [
     tomap({
@@ -7,4 +7,18 @@ output "linux_server" {
       "pass" = azurerm_linux_virtual_machine.linux_node.*.admin_password,
     })
   ]
+} */
+
+output "cockpit_server_user" {
+  value = azurerm_linux_virtual_machine.linux_node.*.admin_username
+}
+
+output "cockpit_server_password" {
+  value     = azurerm_linux_virtual_machine.linux_node.*.admin_password
+  sensitive = true
+}
+
+output "cockpit_webui_url" {
+  value       = format("https://%s:9090", azurerm_linux_virtual_machine.linux_node.*.public_ip_address)
+  description = "Cockpit Web UI URL"
 }
