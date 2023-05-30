99349d8788b1ef618be137248e014541395f3a17
Aki Liu <aki.liu@suse.com>
2023-05-30 17:51:32 +0800

    update

--- scenarios/azure/rancher-devsecops/provider.tf
+++ scenarios/azure/rancher-devsecops/provider.tf
@@ -1,9 +1,11 @@
 provider "azurerm" {
   features {}
+
   subscription_id = var.azure_subscription_id
   tenant_id       = var.azure_tenant_id
   client_id       = var.azure_client_id
   client_secret   = var.azure_client_secret
+  
 }
 
 provider "tls" {
