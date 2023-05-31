19fd0f8d09d271364cc48c34ba73639fb501fd8c
Aki Liu <aki.liu@suse.com>
2023-05-31 10:10:20 +0800

    output

--- scenarios/azure/linux-server-cockpit/output.tf
+++ scenarios/azure/linux-server-cockpit/output.tf
@@ -10,7 +10,7 @@
 } */
 
 output "linux_server_cockput_url" {
-  value       = [for vm in azurerm_linux_virtual_machine.linux_node.* : "https://${vm.name}.${vm.public_ip_address}.sslip.io:9090"]
+  value       = [for vm in azurerm_linux_virtual_machine.linux_node.* : "https://${vm.computer_name}.${vm.public_ip_address}.sslip.io:9090"]
   description = "Linux Node Cockpit Web UI URL"
 } 
 
